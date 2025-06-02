3a92d5895b64884ad05d558a5e2781510ece08a8
bskoon <acedevil728@gmail.com>
2025-06-02 15:12:36 +0900

    new feature6: getSumSum

--- src/test/java/CalTest.java
+++ src/test/java/CalTest.java
@@ -1,2 +1,15 @@
-public class CalTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CalTest {
+
+    @Test
+    void getSumSum() {
+        Cal calculator = new Cal();
+        int expected = 1+2+3;
+
+        int result = calculator.getSumSum(1,2,3);
+
+        assertEquals(expected, result);
+    }
 }
